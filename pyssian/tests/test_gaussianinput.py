35b6fdf9c4739e96a75b2a0bd295ae0cc9b2914d
Raúl Pérez-Soto <rperezsoto.research@gmail.com>
2026-02-10 13:09:11 +0100

    fixed and updated GIF test

--- pyssian/tests/test_gaussianinput.py
+++ pyssian/tests/test_gaussianinput.py
@@ -7,19 +7,22 @@ TEST_FILEDIR = Path(__file__).parent.resolve() / 'test_files'
 class GaussianInFileTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.testfiles = TEST_FILEDIR.glob('gaussian_input_*.{gjf,com}')
+        cls.testfiles = (list(TEST_FILEDIR.glob('gaussian_input_*.com')) +
+                         list(TEST_FILEDIR.glob('gaussian_input_*.gjf')) )
         cls.files_as_text = []
-        for gaufile in cls.testfiles: 
+        for gaufile in cls.testfiles:
             with open(gaufile,'r') as F: 
                 cls.files_as_text.append(F.read())
 
-    def test_read_str_equal(self):
-        msg = 'Read file does not match its str representation'
-        for gaufile,sol in zip(self.testfiles,self.files_as_text):
-            with GaussianInFile(gaufile) as GIF: 
-                GIF.read()
-            test = str(GIF)
-            self.assertEqual(test,sol,msg)
+    def test_read_without_error(self):
+        # Ensure that there is no error during the GaussianInput file 
+        # reading. An error in this test indicates a wrong assumption 
+        # about the valid structure of a gaussian input file. Note 
+        # that if gaussian throws an error, it should throw an error.
+        for i,gaufile in enumerate(self.testfiles):
+            with self.subTest(testfile=i):
+                with GaussianInFile(gaufile) as GIF: 
+                    GIF.read()
 
     def test_nprocshared(self): 
         GIF = GaussianInFile()
